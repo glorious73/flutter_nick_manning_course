6012564c82f7efa845e5d54cbc8c7a2e33860ccb
Amjad Abujamous <amjadmh73@gmail.com>
2023-12-12 17:56:48 +0300

    tile to be used in list

---
+++ lib/components/location_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_seenickcode_one/models/location.dart';
+import 'package:flutter_seenickcode_one/styles.dart';
+
+const LocationTileHeight = 100.0;
+
+class LocationTile extends StatelessWidget {
+  final Location location;
+  final bool darkTheme;
+
+  LocationTile({required this.location, required this.darkTheme});
+
+  @override
+  Widget build(BuildContext context) {
+    final title = location.name.toUpperCase();
+    final subTitle = location.user_itinerary_summary.toUpperCase();
+    final caption = location.tour_package_name.toUpperCase();
+    return Container(
+      padding: EdgeInsets.all(0.0),
+      height: LocationTileHeight,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('$title',
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+                style: this.darkTheme
+                    ? Styles.locationTileTitleDark
+                    : Styles.locationTileTitleLight),
+            Text('$subTitle', style: Styles.locationTileSubTitle),
+            Text('$caption', style: Styles.locationTileCaption),
+          ]),
+    );
+  }
+}
